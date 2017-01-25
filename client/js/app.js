0459ef266487fdc797f21460c1c584994c559c0e
David Weber <davidjwe33@gmail.com>
2017-01-24 17:16:43 -0800

    set up file structure with a server and client side materials. Installed npm packages and other dependencies

---
+++ client/js/app.js
@@ -0,0 +1,12 @@
+angular.module('github-api', ['ui.router'])
+
+  .config(function ($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/')
+
+    $stateProvider
+      .state('search', {
+        url: '/',
+        templateUrl: 'templates/search.html'
+      })
+    })
